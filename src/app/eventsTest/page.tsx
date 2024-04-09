eb011573f9d819b3176a0896cd5044e20f4ea150
LunaMeerkats <luna.meerkats@gmail.com>
2024-04-09 10:49:05 +1000

    SearchBarComponent with example usage

---
+++ src/app/eventsTest/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import EventsComponent from "../../components/EventsComponent";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Test</h1>
+      <EventsComponent />
+    </div>
+  );
+}
